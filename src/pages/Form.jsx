7ad55c4cfd05ddcc8e1322e946c11fe60304d61e
Jawad Messaoudene <[jawadmessaoudene@gmail.com]>
2023-05-11 14:12:41 +0200

    1

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,9 +1,13 @@
+import UsersCard from "../components/UsersCard";
+import { ToastContainer } from "react-toastify";
+
 const Form = () => {
   return (
     <>
       <h1>FORM</h1>
-     
+      <UsersCard></UsersCard>
+      <ToastContainer />
     </>
-  )
+  );
 };
 export default Form;
