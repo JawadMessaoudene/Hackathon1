0cf7b5d5c96a49b576e5bb0aefd6fad8814abcc9
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-05-12 12:20:02 +0200

    Fixed userCards and Home css

--- src/MainRoutes.jsx
+++ src/MainRoutes.jsx
@@ -5,19 +5,26 @@ import { Header } from "./Header"
 import ContextUserInfo from "./contexts/ContextUserInfo";
 import { useState } from "react";
 import Map from "./pages/Map";
+import ContextGuides from "./contexts/ContextGuides";
 
 const MainRoutes = () => {
   const [userInformation, setUserInformation] = useState({});
+  const [ guideInformation, setGuideinformation] = useState()
+
   return (
     <>
-      <ContextUserInfo.Provider value={{userInformation, setUserInformation}}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="/map" element={<Map />} />
-        </Routes>
-      </ContextUserInfo.Provider>
+      <ContextGuides.Provider value={{ guideInformation, setGuideinformation}}>
+        <ContextUserInfo.Provider
+          value={{ userInformation, setUserInformation }}
+        >
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/map" element={<Map />} />
+          </Routes>
+        </ContextUserInfo.Provider>
+      </ContextGuides.Provider>
     </>
   );
 }
